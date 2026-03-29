239a8b135beaecbf4b813908f53dadf32cdea468
Pladniti Phomtakai <167268137+ITiti123npc1npg@users.noreply.github.com>
2026-03-29 12:03:44 +0700

    add

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const multer = require('multer')
+
+
+
+const storage = multer({storage})
+
 
 
 
 const app = express();
-
-
 const port = 8080
-
 
 
 
@@ -15,6 +18,9 @@ app.get('/',(req,res)=>{
     res.json({message:"Hello World"})
 })
 
+app.post('/api/upload',multer.diskStorage('test'),(req,res)=>{
+    res.json({message:"complete"})
+})
 app.listen(port,()=>{
     console.log(`server running is http://localhost:${port}`)
 })
